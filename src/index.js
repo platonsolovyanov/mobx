50b5e2de95045f3d8c9b408c2fbf143479d41130
Platon <platonsolovyanov@gmail.com>
2021-08-24 14:36:54 +0300

    extendObservable

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import DevTools from 'mobx-react-devtools';
-import { observable, computed } from 'mobx';
+import { observable, computed, extendObservable } from 'mobx';
 import { observer } from 'mobx-react';
 
+//В mobx нет строгого разделения на store, action, reduse. Фактически у нас всегда есть объект за свойствами которого мы следим а изменяем его свойства при посмощи свойств этого же объекта.
+//Изменяем соществующие свойства, а не создаем копью объекта с новыми значениями.
+//Все что касается стора может хранится в одном классе
+
 const nickName = new class UserNickName {
-  @observable firstName = 'Platon';
-  @observable age = 21;
+
+  constructor(){ extendObservable(this, {
+    firstName : 'Platon',
+    age : 21
+  })}
+
+  // @observable firstName = 'Platon';
+  // @observable age = 21;
 
   @computed get nickName() {
     console.log('Generate nickName');
@@ -33,7 +43,7 @@ nickName.decrement = function() {
     <div className="App">
       <DevTools />
       <h1>{this.props.store.nickName}</h1>
-      <h1>{this.props.store.count}</h1>
+      <h1>{this.props.store.age}</h1>
       <button onClick={this.handleDecrement}>-1</button>
       <button onClick={this.handleIncrement}>+1</button>
     </div>
